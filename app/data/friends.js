be23caa10850343e2d3cea4eab454b3c8f5877ea
Sally Rae <sally.cardinale@gmail.com>
2019-04-27 17:08:28 -0700

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,68 @@
+const friendData = [{
+    "name": "Mila",
+    "photo": "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTU3NTQ0NzEyMjE4MDkzMjIy/mila-kunis-attends-the-screening-of-the-spy-who-dumped-me-at-hearst-tower-on-july-12-2018-in-new-york-city-photo-by-dimitrios-kambouris_getty-images-square.jpg",
+    "scores": [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Ashton",
+    "photo": "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjN07zoqe_hAhWBFHwKHcvXD-oQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.popsugar.com%2Fcelebrity%2FAshton-Kutcher-Hot-Pictures-33896758&psig=AOvVaw0hiUmzmo86uOj3bqZfoyJJ&ust=1556421973694207",
+    "scores": [
+      3,
+      5,
+      2,
+      2,
+      1,
+      5,
+      1,
+      1,
+      2,
+      3
+    ]
+  },
+  {
+    "name": "Sally",
+    "photo": "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiMyrmPqu_hAhWLv1QKHe3VAXcQjRx6BAgBEAQ&url=http%3A%2F%2Fgoddamestyle.blogspot.com%2F2014%2F12%2Fwhen-harry-met-sally.html&psig=AOvVaw3_FSt7da7x8qfZogqy9oFC&ust=1556422031594627",
+    "scores": [
+      1,
+      2,
+      3,
+      5,
+      1,
+      2,
+      3,
+      4,
+      5,
+      1
+    ]
+  },
+  {
+    "name": "Harry",
+    "photo": "http://images4.fanpop.com/image/photos/21800000/Harry-when-harry-met-sally-21831634-500-274.jpg",
+    "scores": [
+      5,
+      4,
+      3,
+      2,
+      1,
+      5,
+      4,
+      3,
+      2,
+      1
+    ]
+  },
+]
+
+// This allows the server.js file to access the friendData array
+module.exports = friendData;
---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+var nodemon = require("nodemon");
+
+// Setting up express
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Express data parsing
+app.use(express.static('app/public'));
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+// Router
+
+const htmlRouter = require('./app/routing/htmlRoutes.js');
+const apiRouter = require('./app/routing/apiRoutes.js');
+
+app.use(htmlRouter);
+app.use(apiRouter);
+
+
+// Listener
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
